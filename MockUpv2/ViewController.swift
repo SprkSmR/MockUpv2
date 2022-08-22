a662df9d8ae9d9adfd0be9c69f4a5703a1744559
SprkSmR <chitox_ji_armen@hotmail.com>
2022-08-22 17:57:37 -0500

    Initial Commit

---
+++ MockUpv2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockUpv2
+//
+//  Created by ITESM on 22/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
